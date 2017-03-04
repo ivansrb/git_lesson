62c36355615d0e59b0d304153a711b05e00d73f5
Ivan Ilic <00ivan.ilic00@gmail.com>
2017-03-04 07:25:02 +0100

    changed 3 to 4

--- src/my_square.py
+++ src/my_square.py
@@ -1,8 +1,8 @@
 
 def my_square(x):
-        """"takes a value and returns the squared value.
-        """"
+        """takes a value and returns the squared value.
+        """
         
         return(x ** 2)
 
-print(my_square(3))
+print(my_square(4))
